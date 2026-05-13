673b10e178077c3afb992bf5b4a521f79aebbfd7
Martin Jensen <martinloesethjensen@gmail.com>
2026-05-13 17:42:44 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Martin Jensen on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
